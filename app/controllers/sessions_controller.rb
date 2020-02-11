908c79c5d7b61d70f2d3320f9ec10919a332508f
Phyllis Kan <Phyllis Kan>
2020-02-11 16:52:50 -0600

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.find_by(name: params[:user][:name])
+        if @user.nil?
+            flash[:notice] = "User not found"
+            redirect_to login_url
+        else
+            if @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id
+                flash[:notice] = "Log in successful"
+                redirect_to root_url
+            else
+                flash[:notice] = "Authentication failed"
+                redirect_to login_url
+            end
+        end
+    end
+
+    def destroy
+        session.delete :user_id
+        redirect_to root_url
+    end
+end
